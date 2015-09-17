163a5e33da99d14e77deeeb2f630fd0e335049d1
Andrei Stefana <astearon@yahoo.com>
2015-09-17 19:57:40 +0300

    updatet BaseUnit

--- Uwarcraft/Uwarcraft/BaseUnit.cs
+++ Uwarcraft/Uwarcraft/BaseUnit.cs
@@ -8,6 +8,68 @@ namespace Uwarcraft
     public class BaseUnit
     {
         private int unitCost;
-        private int unitProprieties;
+        private int unitHealth;
+        private int unitSpeed;
+        private int unitDamage;
+
+        public int moveUnit
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public int stopUnit
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public int attack
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public int guard
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public int dismiss
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
     }
 }
