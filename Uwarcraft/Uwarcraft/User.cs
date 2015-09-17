85c4cce8b4dc85c2c4a22f4d2ddc331104e1167f
progRobert <prog_robert@yahoo.com>
2015-09-17 20:43:36 +0300

    Login

---
+++ Uwarcraft/Uwarcraft/User.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uwarcraft
+{
+   public class User
+    {
+        string name;
+        string password;
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+
+            set
+            {
+                name = value;
+            }
+        }
+
+        public string Password
+        {
+            get
+            {
+                return password;
+            }
+
+            set
+            {
+                password = value;
+            }
+        }
+    }
+}
