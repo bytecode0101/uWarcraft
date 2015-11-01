1285377b220c41a0f884eed1504b8242d7ef415c
andrei patrascu <andreipatrascu@outlook.com>
2015-11-01 10:05:34 +0200

    testing serialization! warning! the link for XML file used (F.xml) may need to be changed for this to work on another computer.

--- Uwarcraft/Game/DemoGame.cs
+++ Uwarcraft/Game/DemoGame.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 using Uwarcraft.Game;
 using Uwarcraft.Game.StateMachine;
 
@@ -12,8 +14,21 @@ namespace DemoGame
     {
         static void Main(string[] args)
         {
+            XmlSerializer deserializer = new XmlSerializer(typeof(UIBLC));
+            // WARNING !!! You might need to change this link in order to make this project work
+            TextReader reader = new StreamReader(@"C:\Users\Andrei\Source\Repos\uWarcraft\Uwarcraft\Uwarcraft\bin\Debug\F.xml");
+            object obj = deserializer.Deserialize(reader);
+            UIBLC uW = (UIBLC)obj;
+            reader.Close();
+            Console.WriteLine(uW.buildingTypes[2]+" "+uW.buildingTypes[3]+" "+uW.unitTypes[1]+uW.buildingTypes.Length+" "+uW.unitTypes.Length);
+            Console.ReadLine();
             Game g = new Game(new MainMenuState());
             Console.ReadLine();
         }
+    }
+    public class UIBLC
+    {
+        public string[] buildingTypes { get; set; }
+        public string[] unitTypes { get; set; }
     }
 }
---
+++ Uwarcraft/Serialization/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using Uwarcraft;
+using Uwarcraft.Units;
+
+namespace Serialization
+{
+    public class Program
+    {
+        //[XmlInclude(typeof(BuildFarmCapability)), XmlInclude(typeof(BuildBarrackCapability)), XmlInclude(typeof(BuildBowWorkshopCapability)), XmlInclude(typeof(BuildTowerCapability)), XmlInclude(typeof(BuildPeasantCapability)), XmlInclude(typeof(BuildArcherCapability))]
+        static void Main(string[] args)
+        {
+            var buildingTypes = new string[4] {"Farm", "Barack","BowWorkshop","Tower" };
+            var unitTypes = new string[2] { "Peasant", "Archer" };
+            UIBLC uW = new UIBLC();
+            //var buildingTypes = new AbstractBuildBuildingCapability[4];
+            //buildingTypes[0] = new BuildFarmCapability();
+            //buildingTypes[1] = new BuildBarrackCapability();
+            //buildingTypes[2] = new BuildBowWorkshopCapability();
+            //buildingTypes[3] = new BuildTowerCapability();
+            //var unitTypes = new AbstractBuildUnitCapability[2];
+            //unitTypes[0] = new BuildPeasantCapability();
+            //unitTypes[1] = new BuildArcherCapability();
+            //UIBLC uW = new UIBLC();
+            uW.buildingTypes = buildingTypes;
+            uW.unitTypes = unitTypes;
+            Serialize(uW);
+        }
+        //[XmlInclude(typeof(BuildFarmCapability)), XmlInclude(typeof(BuildBarrackCapability)), XmlInclude(typeof(BuildBowWorkshopCapability)), XmlInclude(typeof(BuildTowerCapability)), XmlInclude(typeof(BuildPeasantCapability)), XmlInclude(typeof(BuildArcherCapability))]
+        static public void Serialize(UIBLC uW)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(UIBLC));
+            // WARNING !!! You might need to change this link in order to make this project work
+            using (TextWriter writer = new StreamWriter(@"C:\Users\Andrei\Source\Repos\uWarcraft\Uwarcraft\Uwarcraft\bin\Debug\F.xml")) 
+            {
+                serializer.Serialize(writer, uW);
+            }
+        }        
+    }
+
+    public class UIBLC
+    {
+        public string[] buildingTypes { get; set; }
+        public string[] unitTypes { get; set; }
+        //public AbstractBuildBuildingCapability[] buildingTypes { get; set; }
+        //public AbstractBuildUnitCapability[] unitTypes { get; set; }
+    }
+}
